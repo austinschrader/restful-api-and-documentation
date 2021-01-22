581e1cb3f85cab4bb8a0ffab5087fa239863d202
Austin Schrader <austin.jschrader@gmail.com>
2021-01-22 12:37:51 -0800

    Add restaurant model properties

--- BusinessApi/Models/Restaurant.cs
+++ BusinessApi/Models/Restaurant.cs
@@ -1,8 +1,21 @@
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace BusinessApi.Models
 {
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
 
+    [Required]
+    [DisplayName("Enter the Restaurant's name")]
+    public string Name { get; set; }
+
+    [Required]
+    [DisplayName("Enter the Restaurant's pricing")]
+    [Range(1, 3, ErrorMessage = "Pricing must be on a 1-3 scale.")]
+    public int Pricing { get; set; }
+  }
 }
