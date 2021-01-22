0266da27072d0d3e4eadbac7cdb195b82df116f7
Austin Schrader <austin.jschrader@gmail.com>
2021-01-22 12:15:51 -0800

    Update businessapicontext with models

--- BusinessApi/Models/BusinessApiContext.cs
+++ BusinessApi/Models/BusinessApiContext.cs
@@ -2,5 +2,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BusinessApi.Models
 {
+  public class BusinessApiContext : DbContext
+  {
+    public BusinessApiContext(DbContextOptions<BusinessApiContext> options) : base(options) { }
 
+    public virtual DbSet<Restaurant> Restaurants { get; set; }
+    public virtual DbSet<Shop> Shops { get; set; }
+  }
 }
