31a78892ec58dce6dc33f48733058d0cbecb7e97
Austin Schrader <austin.jschrader@gmail.com>
2021-01-22 16:24:21 -0800

    Update shopscontroller with get all

--- BusinessApi/Controllers/ShopsController.cs
+++ BusinessApi/Controllers/ShopsController.cs
@@ -4,3 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 using BusinessApi.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+
+namespace BusinessApi.Contollers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ShopsController : ControllerBase
+  {
+
+    private BusinessApiContext _db;
+    public ShopsController(BusinessApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/Shops
+    [HttpGet]
+    public ActionResult<IEnumerable<Shop>> Get(int pricing, string description)
+    {
+      var query = _db.Shops.AsQueryable();
+      if (pricing != 0)
+      {
+        query = query.Where(entry => entry.Pricing == pricing);
+      }
+      if (description != null)
+      {
+        query = query.Where(entry => entry.Description == description);
+      }
+      return query.ToList();
+    }
+
+  }
+}
